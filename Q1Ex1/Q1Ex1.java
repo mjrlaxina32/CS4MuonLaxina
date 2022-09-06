1217766c1ee193f0d53def6bdcdfc0c23b0c000b
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-09-06 08:57:02 +0800

    Update Q1Ex1.java

--- Q1Ex1/Q1Ex1.java
+++ Q1Ex1/Q1Ex1.java
@@ -1,1 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex1;
 
+/**
+ *
+ * @author MUON
+ */
+public class Q1Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //person 1
+        int p1Comnum = 2;
+        String name = "MJ Laxina";
+        
+        System.out.println("Person 1 /n Computer number:  /n Name: ");
+    }
+    
+}
