425acb855eeb08736277fbd17ac90c46383540c8
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-09-08 11:05:07 +0800

    Update Q1Ex1.java
    partial - display done

--- Q1Ex1/Q1Ex1.java
+++ Q1Ex1/Q1Ex1.java
@@ -19,11 +19,39 @@ public class Q1Ex1 {
         
         //Monday Subj Periods
         //Subj 1
-        int periods = 2;
-        String name = "CompSci";
-        String teacher = "Sir. Mark Tarcelo";
+        int periods1 = 2;
+        String name1 = "CompSci";
+        String teacher1 = "Sir. Tarcelo";
         
-        //System.out.println("");
+        //Subj 2
+        int periods2 = 1;
+        String name2 = "ValEd";
+        String teacher2 = "Ma'am Briones";
+        
+        //Subj 3
+        int periods3 = 1;
+        String name3 = "English";
+        String teacher3 = "Ma'am Bulan";
+        
+        System.out.println("Subject 1");
+        System.out.println("Periods: " + periods1);
+        System.out.println("Subject Name: " + name1);
+        System.out.println("Subject Teacher: " + teacher1);
+        System.out.println(" ");
+        
+        System.out.println("Subject 2");
+        System.out.println("Periods: " + periods2);
+        System.out.println("Subject Name: " + name2);
+        System.out.println("Subject Teacher: " + teacher2);
+        System.out.println(" ");
+        
+        System.out.println("Subject 3");
+        System.out.println("Periods: " + periods3);
+        System.out.println("Subject Name: " + name3);
+        System.out.println("Subject Teacher: " + teacher3);
+        System.out.println(" ");
+        
+        
     }
     
 }
