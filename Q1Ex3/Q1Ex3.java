e1b6eff413e57f9370433d927b35aebcb25b426a
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-09-24 23:56:44 +0800

    Update Q1Ex3.java

--- Q1Ex3/Q1Ex3.java
+++ Q1Ex3/Q1Ex3.java
@@ -14,14 +14,23 @@ public class Q1Ex3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        class MonSubs {
-        }
-        
-        class Song {
-        }
-        
-        class Singer {
-        }
+        //CLASS - SUBJECTS
+            Subjects sub1 = new Subjects(2, "Sir. Tarcelo", "CompSci", "Monday");
+            Subjects sub2 = new Subjects(1, "Ma'am Bulan", "English", "Monday");
+            Subjects sub3 = new Subjects(2, "Sir. Neil", "Physics", "Friday");
+
+        //SONG
+            Song song1 = new Song(4.34, "Marilag", "Munimuni", "Simula", true);
+            Song song2 = new Song(3.41, "Mercy", "Shawn Mendez", "Illuminate", false);
+
+        //SINGER
+            Song song3 = new Song(3.41, "Ligaya", "Eraserheads", "Ultramagneticpop!", true);
+
+            Singer singer1 = new Singer("Mang Aawit");
+            singer1.setFavSong(song1);
+            singer1.performForAudience(10);
+            singer1.setFavSong(song3);
+            singer1.performForAudience(2);
     }
     
 }
