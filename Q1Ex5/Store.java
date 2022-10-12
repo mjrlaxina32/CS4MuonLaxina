9d12ee3835d1223740578f2dd3b437e7a78550e3
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-10-12 08:35:49 +0800

    Update Store.java

--- Q1Ex5/Store.java
+++ Q1Ex5/Store.java
@@ -20,6 +20,7 @@ public class Store {
   public Store(String name){
     this.name = name;
     ArrayList<Item> itemList = new ArrayList<>();// Initialize itemList as a new ArrayList
+    this.itemList = itemList;
     storeList.add(this);// add 'this' store to storeList
   }
 
@@ -30,11 +31,11 @@ public class Store {
     return earnings;
   }
   public void sellItem(int index){
-    if ((index < 0 || index >= itemList.size())){
-        System.out.println("There are only" + itemList.size() + "items in the store.\n");
+    if ((index < 0 || index >= this.itemList.size())){
+        System.out.println("There are only " + this.itemList.size() + " items in the store.\n");
     }
     else {
-        Item soldItem = itemList.get(index);
+        Item soldItem = this.itemList.get(index);
         earnings += soldItem.getCost();
         System.out.println("You sold "+soldItem.getName()+" for "+soldItem.getCost()+". You now have "+earnings+".\n");
     }
@@ -45,10 +46,11 @@ public class Store {
   public void sellItem(String name){
     Item soldItem = null;
     boolean isFound = false;
-    for (Item e : itemList){
-        if(e.getName().equals(name)){
+    for (Item e : this.itemList){
+        if(e.getName().equalsIgnoreCase(name)){
             soldItem = e;
             isFound = true;
+            break;
         }
         else {
             isFound = false;
@@ -61,56 +63,68 @@ public class Store {
         earnings += soldItem.getCost();
         System.out.println("You sold "+soldItem.getName()+" for "+soldItem.getCost()+". You now have "+earnings+".\n");
     }
-  }
     // check if Item with given name is in the itemList (you will need to loop over itemList) (if not, print statement that the store doesn't sell it)
     // get Item from itemList and add its cost to earnings
     // print statement indicating the sale
+  }
+    
   public void sellItem(Item i){
     Item soldItem;
-    if (itemList.contains(i)){
+    if (this.itemList.contains(i)){
         soldItem = i;
         earnings += soldItem.getCost();
         System.out.println("You sold "+soldItem.getName()+" for "+soldItem.getCost()+". You now have "+earnings+".\n");
     }
     else {
-        System.out.println("This store does not sell " + i + ". \n");
+        System.out.println("This store does not sell " + i.getName() + ". \n");
     }
     // check if Item i exists in the store (there is a method that can help with this) (if not, print statement that the store doesn't sell it)
     // get Item i from itemList and add its cost to earnings
     // print statement indicating the sale
   }
+  
   public void addItem(Item i){
-      itemList.add(i); //WHY WONT THIS WO
-      // add Item i to store's itemList
+    this.itemList.add(i);
+    // add Item i to store's itemList
   }
+  
   public void filterType(String type){
-    for (Item e : itemList){
+    for (Item e : this.itemList){
         if(e.getType().equals(type)){
-            System.out.println(e + "|");
+            System.out.println(e.getName());
         }
     }
+    System.out.println("\n");
     // loop over itemList and print all items with the specified type
   }
+  
   public void filterCheap(double maxCost){
-    for (Item e : itemList){
+    //ArrayList<Item> itemList = new ArrayList<>();
+    for (Item e : this.itemList){
         if(e.getCost() <= maxCost){
-            System.out.println(e + "|");
+            System.out.println(e.getName());
         }
     }
+    System.out.println("\n");
     // loop over itemList and print all items with a cost lower than or equal to the specified value
   }
+  
   public void filterExpensive(double minCost){
-    for (Item e : itemList){
+    //ArrayList<Item> itemList = new ArrayList<>();
+    for (Item e : this.itemList){
         if(e.getCost() >= minCost){
-            System.out.println(e + "|");
+            System.out.println(e.getName());
         }
     }
-      // loop over itemList and print all items with a cost higher than or equal to the specified value
+    System.out.println("\n");
+    // loop over itemList and print all items with a cost higher than or equal to the specified value
   }
+  
   public static void printStats(){
     // loop over storeList and print the name and the earnings'Store.java'
     for (Store e : storeList){
         System.out.println(e.getName() + " - " + e.getEarnings());
     }
   }
+  
 }
