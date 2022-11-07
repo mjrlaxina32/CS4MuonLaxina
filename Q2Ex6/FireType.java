01bc50b9fb5568f128e865f208c7a0a3f6d9026f
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-11-07 08:54:07 +0800

    Add files via upload

---
+++ Q2Ex6/FireType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise06muonlaxinamoniquejhoienyl;
+
+/**
+ *
+ * @author MUON
+ */
+public class FireType extends Monster {
+    
+    public FireType(String n, int m, int base){
+        super(n, "fire", "grass", "water", m, base);
+        
+    }
+}
