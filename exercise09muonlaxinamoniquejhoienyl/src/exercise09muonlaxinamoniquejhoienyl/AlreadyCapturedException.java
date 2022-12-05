aac0ed9436e3f19e6833cf987204fd1cfc6ffe95
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2022-12-05 08:57:04 +0800

    Add files via upload

---
+++ exercise09muonlaxinamoniquejhoienyl/src/exercise09muonlaxinamoniquejhoienyl/AlreadyCapturedException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise09muonlaxinamoniquejhoienyl;
+
+/**
+ *
+ * @author MUON
+ */
+public class AlreadyCapturedException extends Exception {
+    /**
+     * Creates a new instance of <code>MonsterNotFoundException</code> without
+     * detail message.
+     */
+    public AlreadyCapturedException() {
+    }
+
+    /**
+     * Constructs an instance of <code>MonsterNotFoundException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public AlreadyCapturedException(String msg) {
+        super(msg);
+    }
+}
---
+++ exercise09muonlaxinamoniquejhoienyl/src/exercise09muonlaxinamoniquejhoienyl/FireType.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise09muonlaxinamoniquejhoienyl;
+
+/**
+ *
+ * @author MUON
+ */
+public class FireType extends Monster {
+    public FireType(String n, int m, int base){
+        super(n, "fire", "grass", "water", m, base);
+        atk *= 1.3;
+        def *= 0.7;
+    }
+    
+    @Override
+    public void special(){
+        System.out.println(name + " did a pose!");
+        atk += 2;
+        hp -= maxHP*0.10;
+    }
+}
